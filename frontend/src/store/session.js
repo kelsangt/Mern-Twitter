309b1ec76baf9e88b2d5e0ef0fb4df119b127a06
Kelsang <kelsangrtsering@gmail.com>
2023-04-16 09:42:54 -0400

    work on phase 6 of frontend auth

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -23,6 +23,12 @@ export const clearSessionErrors = () => ({
   type: CLEAR_SESSION_ERRORS
 });
 
+export const getCurrentUser = () => async dispatch => {
+    const res = await jwtFetch('/api/users/current');
+    const user = await res.json();
+    return dispatch(receiveCurrentUser(user));
+};
+
 export const signup = user => startSession(user, 'api/users/register');
 export const login = user => startSession(user, 'api/users/login');
 
